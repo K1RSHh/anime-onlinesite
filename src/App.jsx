c8c4d8060c0b0ae76124e731d72c3dbae33533df
K1RSHh <mark2jzx100ua@gmail.com>
2026-02-02 04:11:43 +0200

    alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
-import Home from "./page/home";
 import Header from "./components/Header/Header";
+import Home from "./page/home";
+import News from "./page/News";
+import Categories from "./page/Categories";
 
 function App() {
   return (
-    <div className="w-full m-5">
+    <div className="w-full h-full m-5">
       <Header />
       <div className="mt-24 max-w-6xl m-auto">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
       </div>
     </div>
---
+++ src/page/News.jsx
@@ -0,0 +1,11 @@
+function News() {
+  return (
+    <div className="flex mt-40">
+      <div className="text-center m-auto items-center">
+        <p className="text-9xl">Coming soon!</p>
+      </div>
+    </div>
+  );
+}
+
+export default News;
